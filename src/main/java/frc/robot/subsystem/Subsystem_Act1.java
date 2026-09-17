d8c3e026b4952166ef010bebe876d523b519d1a3
Ervey Hdz <ervey.hdz.rdz@gmail.com>
2026-09-17 17:18:15 -0600

    Terminé de hacer los comandos

---
+++ src/main/java/frc/robot/subsystem/Subsystem_Act1.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Subsystem_Act1 extends SubsystemBase {
+  /** Creates a new Subsystem_Act1. */
+
+  public Subsystem_Act1() {}
+
+  // Other ways to create commands
+  Command PressButton_1_Option2 = Commands.runOnce(() -> System.out.println("Button Y was pressed"));
+  Command PressButton_2_Option2 = Commands.runOnce(() -> System.out.println("Button B was pressed"));
+  Command PressButton_3_Option2 = Commands.runOnce(() -> System.out.println("Button A was pressed"));
+
+
+  // Real Commands
+  public Command PressButton_1() {
+    return runOnce(() -> System.out.print("Button Y was pressed"));
+  }
+
+  public Command PressButton_2() {
+    return runOnce(() -> System.out.println("Button B was pressed"));
+  }
+
+  public Command PressButton_3() {
+    return runOnce(() -> System.out.println("Button A was pressed"));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
---
+++ src/main/java/frc/robot/subsystem/Subsystem_Act1_Name.java
@@ -0,0 +1,22 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystem;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Subsystem_Act1_Name extends SubsystemBase {
+  /** Creates a new Subsystem_Act1_Name. */
+
+  public Subsystem_Act1_Name() {}
+
+  Command ShowName = Commands.runOnce(() -> System.out.println("Ervey"));
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
